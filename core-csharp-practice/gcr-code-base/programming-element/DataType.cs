5357fb631d2302cf95a04c86fe3a84e2c1d2aad3
bhanu-dagur <bhanudagur1702@gmail.com>
2025-12-19 12:13:24 +0530

    operator & datatype

---
+++ core-csharp-practice/gcr-code-base/programming-element/DataType.cs
@@ -0,0 +1,48 @@
+class DataType
+{
+    public static void Main(String [] args)
+    {
+        // Intege type
+        int a = 10; 
+        long b = 1234567890;
+        short c = 5;
+        byte d = 255;
+
+        // Floating Types
+        float f = 10.5f;
+        double db = 99.99;
+
+        // Boolean
+        bool isPassed = true;
+
+        // Character
+        char grade = 'A';
+
+        // String
+        string name = "Bhanu";
+
+        // Object
+        object obj1 = 100;
+        object obj2 = "Hello";
+
+        // var
+        var num = 20;
+        var text = "CSharp";
+
+        // Output
+        Console.WriteLine("int: " + a);
+        Console.WriteLine("long: " + b);
+        Console.WriteLine("short: " + c);
+        Console.WriteLine("byte: " + d);
+        Console.WriteLine("float: " + f);
+        Console.WriteLine("double: " + db);
+        Console.WriteLine("bool: " + isPassed);
+        Console.WriteLine("char: " + grade);
+        Console.WriteLine("string: " + name);
+        Console.WriteLine("object1: " + obj1);
+        Console.WriteLine("object2: " + obj2);
+        Console.WriteLine("var num: " + num);
+        Console.WriteLine("var text: " + text);
+        
+    }
+}
---
+++ core-csharp-practice/gcr-code-base/programming-element/Operator.cs
@@ -0,0 +1,85 @@
+class Operator
+{
+    static void Main(String [] args)
+    {
+        ArithmeticOperation(2,3);
+        RelationalOperator(2,3);
+       LogicalOperator(false,true);
+        UnaryOperator(5);
+        TernaryOperator(10,12);
+        AssignmentOperator(2,3);
+        IsOperator();
+
+    }
+    static void ArithmeticOperation(int a,int b)
+    {
+        Console.WriteLine("Addition: " + (a + b)); 
+        Console.WriteLine("Subtraction: " + (a - b)); 
+        Console.WriteLine("Multiplication: " + (a * b)); 
+        Console.WriteLine("Division: " + (a / b)); 
+        Console.WriteLine("Modulus: " + (a % b));
+    } 
+    static void  RelationalOperator(int a,int b)
+    {
+        Console.WriteLine(" double equal operation of a==b "+ (a==b));
+        Console.WriteLine("Not equal operation of a!=b" + (a!=b));
+        Console.WriteLine("Greater than operation of a>b" +(a>b));
+        Console.WriteLine("Greater than equal to operation of  a>=b" + (a>=b));
+        Console.WriteLine("Less than  operation of a<b" +(a<b));
+        Console.WriteLine("Less than Equal to operation of a<=b" + (a<=b));
+        
+    }
+    static void LogicalOperator(bool a,bool b)
+    {
+        Console.WriteLine(" And operation of a && b"+ (a&&b));
+        Console.WriteLine("Or operation of a||b"+ (a||b));
+        Console.WriteLine("Logical Not operation ofa + " +(!a));
+    }
+    static void UnaryOperator(int a)
+    {
+        Console.WriteLine("a: " + a);
+        Console.WriteLine("pre-increment operation on a (++a): " + ++a); 
+        Console.WriteLine("prost-increment operation on a (a++): " + a++); 
+        Console.WriteLine("pre decrement operation on a (--a): " + --a); 
+        Console.WriteLine("post decrement operation on a (a--): " + a--); 
+        Console.WriteLine("a: " + a); 
+    }
+    static void AssignmentOperator(int a,int b)
+    {
+        //Addition assignment
+        a += b; 
+        Console.WriteLine("a += b: " + a); 
+
+        //Subtraction assignment
+        a -= b; 
+        Console.WriteLine("a -= b: " + a); 
+
+        //Multiplication assignment
+        a *= b; 
+        Console.WriteLine("a *= b: " + a); 
+
+        //Division assignment
+        a /= b; 
+        Console.WriteLine("a /= b: " + a);
+
+        // Modulus assingment
+        a %= b; 
+        Console.WriteLine("a %= b: " + a); 
+    }
+    static void IsOperator()
+    {
+        object obj = "Bhanu";
+        if (obj is string)
+        {
+            Console.WriteLine("obj is a string");
+        }
+    }
+    static void TernaryOperator(int a,int b)
+    {
+        int max = (a > b) ? a : b;
+        Console.WriteLine("Max value from a and b " + max); 
+
+    }
+
+
+}
